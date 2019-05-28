4af36f4ad64b4ccd8bb8fd356faecaa347ee05ba
Simon Hou <simonhou@vipabc.com>
2019-05-28 11:12:17 +0800

    Update: Format

--- tests/test_player.cpp
+++ tests/test_player.cpp
@@ -23,77 +23,72 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#include <signal.h>
-#include <unistd.h>
-#include "Util/logger.h"
-#include <iostream>
+#include "H264Decoder.h"
+#include "Network/sockutil.h"
+#include "Player/MediaPlayer.h"
 #include "Poller/EventPoller.h"
 #include "Rtsp/UDPServer.h"
-#include "Player/MediaPlayer.h"
+#include "Util/logger.h"
 #include "Util/onceToken.h"
-#include "H264Decoder.h"
 #include "YuvDisplayer.h"
-#include "Network/sockutil.h"
+#include <iostream>
+#include <signal.h>
+#include <unistd.h>
 
 using namespace std;
 using namespace toolkit;
 using namespace mediakit;
 
-int main(int argc, char *argv[]) {
-    //设置退出信号处理函数
-    signal(SIGINT, [](int) { SDLDisplayerHelper::Instance().shutdown(); });
-    //设置日志
-    Logger::Instance().add(std::make_shared<ConsoleChannel>());
-    Logger::Instance().setWriter(std::make_shared<AsyncLogWriter>());
+int main(int argc, char* argv[])
+{
+	//设置退出信号处理函数
+	signal(SIGINT, [](int) { SDLDisplayerHelper::Instance().shutdown(); });
+	//设置日志
+	Logger::Instance().add(std::make_shared<ConsoleChannel>());
+	Logger::Instance().setWriter(std::make_shared<AsyncLogWriter>());
 
-    if (argc != 3) {
-        ErrorL << "\r\n测试方法：./test_player rtxp_url rtp_type\r\n"
-               << "例如：./test_player rtsp://admin:123456@127.0.0.1/live/0 0\r\n"
-               << endl;
-        return 0;
+	if (argc != 3) {
+		ErrorL << "\r\n测试方法：./test_player rtxp_url rtp_type\r\n"
+		       << "例如：./test_player rtsp://admin:123456@127.0.0.1/live/0 0\r\n"
+		       << endl;
+		return 0;
+	}
 
-    }
+	MediaPlayer::Ptr      player(new MediaPlayer());
+	weak_ptr<MediaPlayer> weakPlayer = player;
+	player->setOnPlayResult([weakPlayer](const SockException& ex) {
+		InfoL << "OnPlayResult:" << ex.what();
+		auto strongPlayer = weakPlayer.lock();
+		if (ex || !strongPlayer) {
+			return;
+		}
 
-    MediaPlayer::Ptr player(new MediaPlayer());
-    weak_ptr<MediaPlayer> weakPlayer = player;
-    player->setOnPlayResult([weakPlayer](const SockException &ex) {
-        InfoL << "OnPlayResult:" << ex.what();
-        auto strongPlayer = weakPlayer.lock();
-        if (ex || !strongPlayer) {
-            return;
-        }
+		auto viedoTrack = strongPlayer->getTrack(TrackVideo);
+		if (!viedoTrack || viedoTrack->getCodecId() != CodecH264) {
+			WarnL << "没有视频或者视频不是264编码!";
+			return;
+		}
+		SDLDisplayerHelper::Instance().doTask([viedoTrack]() {
+			std::shared_ptr<H264Decoder>  decoder(new H264Decoder);
+			std::shared_ptr<YuvDisplayer> displayer(new YuvDisplayer);
+			viedoTrack->addDelegate(std::make_shared<FrameWriterInterfaceHelper>([decoder, displayer](const Frame::Ptr& frame) {
+				SDLDisplayerHelper::Instance().doTask([decoder, displayer, frame]() {
+					AVFrame* pFrame = nullptr;
+					bool     flag   = decoder->inputVideo((unsigned char*)frame->data(), frame->size(), frame->stamp(), &pFrame);
+					if (flag) {
+						displayer->displayYUV(pFrame);
+					}
+					return true;
+				});
+			}));
+			return true;
+		});
+	});
 
-        auto viedoTrack = strongPlayer->getTrack(TrackVideo);
-        if (!viedoTrack || viedoTrack->getCodecId() != CodecH264) {
-            WarnL << "没有视频或者视频不是264编码!";
-            return;
-        }
-        SDLDisplayerHelper::Instance().doTask([viedoTrack]() {
-            std::shared_ptr<H264Decoder> decoder(new H264Decoder);
-            std::shared_ptr<YuvDisplayer> displayer(new YuvDisplayer);
-            viedoTrack->addDelegate(std::make_shared<FrameWriterInterfaceHelper>([decoder, displayer](const Frame::Ptr &frame) {
-                SDLDisplayerHelper::Instance().doTask([decoder, displayer, frame]() {
-                    AVFrame *pFrame = nullptr;
-                    bool flag = decoder->inputVideo((unsigned char *) frame->data(), frame->size(),
-                                                    frame->stamp(), &pFrame);
-                    if (flag) {
-                        displayer->displayYUV(pFrame);
-                    }
-                    return true;
-                });
-            }));
-            return true;
-        });
-    });
+	player->setOnShutdown([](const SockException& ex) { ErrorL << "OnShutdown:" << ex.what(); });
+	(*player)[kRtpType] = atoi(argv[2]);
+	player->play(argv[1]);
 
-
-    player->setOnShutdown([](const SockException &ex) {
-        ErrorL << "OnShutdown:" << ex.what();
-    });
-    (*player)[kRtpType] = atoi(argv[2]);
-    player->play(argv[1]);
-
-    SDLDisplayerHelper::Instance().runLoop();
-    return 0;
+	SDLDisplayerHelper::Instance().runLoop();
+	return 0;
 }
-
--- tests/test_pusher.cpp
+++ tests/test_pusher.cpp
@@ -24,15 +24,15 @@
  * SOFTWARE.
  */
 
-#include <signal.h>
-#include <iostream>
-#include "Util/logger.h"
-#include "Util/NoticeCenter.h"
-#include "Poller/EventPoller.h"
-#include "Player/PlayerProxy.h"
-#include "Rtmp/RtmpPusher.h"
 #include "Common/config.h"
+#include "Player/PlayerProxy.h"
+#include "Poller/EventPoller.h"
 #include "Pusher/MediaPusher.h"
+#include "Rtmp/RtmpPusher.h"
+#include "Util/NoticeCenter.h"
+#include "Util/logger.h"
+#include <iostream>
+#include <signal.h>
 
 using namespace std;
 using namespace toolkit;
@@ -40,84 +40,83 @@ using namespace mediakit;
 
 //推流器，保持强引用
 MediaPusher::Ptr pusher;
-Timer::Ptr g_timer;
+Timer::Ptr       g_timer;
 
 //声明函数
-void rePushDelay(const EventPoller::Ptr &poller,const string &schema,const string &vhost,const string &app, const string &stream, const string &url);
+void rePushDelay(const EventPoller::Ptr& poller, const string& schema, const string& vhost, const string& app, const string& stream, const string& url);
 
 //创建推流器并开始推流
-void createPusher(const EventPoller::Ptr &poller, const string &schema,const string &vhost,const string &app, const string &stream, const string &url) {
-    //创建推流器并绑定一个MediaSource
-    pusher.reset(new MediaPusher(schema,vhost, app, stream,poller));
-    //可以指定rtsp推流方式，支持tcp和udp方式，默认tcp
-//    (*pusher)[Client::kRtpType] = Rtsp::RTP_UDP;
-    //设置推流中断处理逻辑
-    pusher->setOnShutdown([poller,schema,vhost, app, stream, url](const SockException &ex) {
-        WarnL << "Server connection is closed:" << ex.getErrCode() << " " << ex.what();
-        //重试
-        rePushDelay(poller,schema,vhost,app, stream, url);
-    });
-    //设置发布结果处理逻辑
-    pusher->setOnPublished([poller,schema,vhost, app, stream, url](const SockException &ex) {
-        if (ex) {
-            WarnL << "Publish fail:" << ex.getErrCode() << " " << ex.what();
-            //如果发布失败，就重试
-            rePushDelay(poller,schema,vhost,app, stream, url);
-        } else {
-            InfoL << "Publish success,Please play with player:" << url;
-        }
-    });
-    pusher->publish(url);
+void createPusher(const EventPoller::Ptr& poller, const string& schema, const string& vhost, const string& app, const string& stream, const string& url)
+{
+	//创建推流器并绑定一个MediaSource
+	pusher.reset(new MediaPusher(schema, vhost, app, stream, poller));
+	//可以指定rtsp推流方式，支持tcp和udp方式，默认tcp
+	//    (*pusher)[Client::kRtpType] = Rtsp::RTP_UDP;
+	//设置推流中断处理逻辑
+	pusher->setOnShutdown([poller, schema, vhost, app, stream, url](const SockException& ex) {
+		WarnL << "Server connection is closed:" << ex.getErrCode() << " " << ex.what();
+		//重试
+		rePushDelay(poller, schema, vhost, app, stream, url);
+	});
+	//设置发布结果处理逻辑
+	pusher->setOnPublished([poller, schema, vhost, app, stream, url](const SockException& ex) {
+		if (ex) {
+			WarnL << "Publish fail:" << ex.getErrCode() << " " << ex.what();
+			//如果发布失败，就重试
+			rePushDelay(poller, schema, vhost, app, stream, url);
+		}
+		else {
+			InfoL << "Publish success,Please play with player:" << url;
+		}
+	});
+	pusher->publish(url);
 }
 
 //推流失败或断开延迟2秒后重试推流
-void rePushDelay(const EventPoller::Ptr &poller,const string &schema,const string &vhost,const string &app, const string &stream, const string &url) {
-    g_timer = std::make_shared<Timer>(2,[poller,schema,vhost,app, stream, url]() {
-        InfoL << "Re-Publishing...";
-        //重新推流
-        createPusher(poller,schema,vhost,app, stream, url);
-        //此任务不重复
-        return false;
-    }, poller);
+void rePushDelay(const EventPoller::Ptr& poller, const string& schema, const string& vhost, const string& app, const string& stream, const string& url)
+{
+	g_timer = std::make_shared<Timer>(2,
+					  [poller, schema, vhost, app, stream, url]() {
+						  InfoL << "Re-Publishing...";
+						  //重新推流
+						  createPusher(poller, schema, vhost, app, stream, url);
+						  //此任务不重复
+						  return false;
+					  },
+					  poller);
 }
 
 //这里才是真正执行main函数，你可以把函数名(domain)改成main，然后就可以输入自定义url了
-int domain(const string &playUrl, const string &pushUrl) {
-    //设置日志
-    Logger::Instance().add(std::make_shared<ConsoleChannel>());
-    Logger::Instance().setWriter(std::make_shared<AsyncLogWriter>());
-    auto poller = EventPollerPool::Instance().getPoller();
+int domain(const string& playUrl, const string& pushUrl)
+{
+	//设置日志
+	Logger::Instance().add(std::make_shared<ConsoleChannel>());
+	Logger::Instance().setWriter(std::make_shared<AsyncLogWriter>());
+	auto poller = EventPollerPool::Instance().getPoller();
 
-    //拉一个流，生成一个RtmpMediaSource，源的名称是"app/stream"
-    //你也可以以其他方式生成RtmpMediaSource，比如说MP4文件（请查看test_rtmpPusherMp4.cpp代码）
-    PlayerProxy::Ptr player(new PlayerProxy(DEFAULT_VHOST, "app", "stream",false,false,-1 , poller));
-    player->play(playUrl.data());
+	//拉一个流，生成一个RtmpMediaSource，源的名称是"app/stream"
+	//你也可以以其他方式生成RtmpMediaSource，比如说MP4文件（请查看test_rtmpPusherMp4.cpp代码）
+	PlayerProxy::Ptr player(new PlayerProxy(DEFAULT_VHOST, "app", "stream", false, false, -1, poller));
+	player->play(playUrl.data());
 
-    //监听RtmpMediaSource注册事件,在PlayerProxy播放成功后触发
-    NoticeCenter::Instance().addListener(nullptr, Broadcast::kBroadcastMediaChanged,
-                                         [pushUrl,poller](BroadcastMediaChangedArgs) {
-                                             //媒体源"app/stream"已经注册，这时方可新建一个RtmpPusher对象并绑定该媒体源
-                                             if(bRegist && pushUrl.find(schema) == 0){
-                                                 createPusher(poller,schema,vhost,app, stream, pushUrl);
-                                             }
-                                         });
+	//监听RtmpMediaSource注册事件,在PlayerProxy播放成功后触发
+	NoticeCenter::Instance().addListener(nullptr, Broadcast::kBroadcastMediaChanged, [pushUrl, poller](BroadcastMediaChangedArgs) {
+		//媒体源"app/stream"已经注册，这时方可新建一个RtmpPusher对象并绑定该媒体源
+		if (bRegist && pushUrl.find(schema) == 0) {
+			createPusher(poller, schema, vhost, app, stream, pushUrl);
+		}
+	});
 
-    //设置退出信号处理函数
-    static semaphore sem;
-    signal(SIGINT, [](int) { sem.post(); });// 设置退出信号
-    sem.wait();
-    pusher.reset();
-    g_timer.reset();
-    return 0;
+	//设置退出信号处理函数
+	static semaphore sem;
+	signal(SIGINT, [](int) { sem.post(); }); // 设置退出信号
+	sem.wait();
+	pusher.reset();
+	g_timer.reset();
+	return 0;
 }
 
-
-int main(int argc, char *argv[]) {
-    return domain("rtmp://live.hkstv.hk.lxdns.com/live/hks1", "rtsp://127.0.0.1/live/rtsp_push");
+int main(int argc, char* argv[])
+{
+	return domain("rtmp://live.hkstv.hk.lxdns.com/live/hks1", "rtsp://127.0.0.1/live/rtsp_push");
 }
-
-
-
-
-
-
